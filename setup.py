897457ec3b3eaaeb32cad638aad28fb7c86b51f1
Reno Elmendorp <reno@Renos-MBP.dynamic.ziggo.nl>
2019-05-18 14:07:43 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+from setuptools import setup, find_packages
+
+# dependencies; currently none
+dependencies = []
+
+# add enum34 package if Python < 3.4
+if sys.version_info < (3, 4):
+    dependencies.append('enum34')
+
+setup(
+    name='avlwrapper',
+    version='0.1',
+    url='https://github.com/renoelmendorp/AVLWrapper',
+    author='Reno Elmendorp',
+    license='LICENCE',
+    packages=['avlwrapper'],
+    install_requires=dependencies
+)
