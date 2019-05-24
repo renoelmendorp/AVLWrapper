c6e539711eb694c999ee30c5078a2aa4559e6159
Reno Elmendorp <R.J.M.Elmendorp@tudelft.nl>
2019-05-24 17:18:41 +0200

    Moved parsing methods to functions outside the OutputReader class

--- avlwrapper/output.py
+++ avlwrapper/output.py
@@ -1,13 +1,15 @@
 import os.path
 import re
 
+SURFACE_RE = 'Surface\s+#\s*\d+\s+(.*)'
+STRIP_RE = 'Strip\s+#\s+(\d+)\s+'
+
+
 class ParseError(Exception):
     pass
 
-class OutputReader(object):
 
-    SURFACE_RE = 'Surface\s+#\s*\d+\s+(.*)'
-    STRIP_RE = 'Strip\s+#\s+(\d+)\s+'
+class OutputReader(object):
 
     """Reads AVL output files. Type is determined based on file extension"""
     def __init__(self, file_path):
@@ -20,183 +22,193 @@ class OutputReader(object):
             content = out_file.readlines()
 
         if self.extension == '.ft':
-            result = self._read_totals(content)
+            result = read_totals(content)
         elif self.extension == '.fn':
-            result = self._read_surface_forces(content)
+            result = read_surface_forces(content)
         elif self.extension == '.fs':
-            result = self._read_strip_forces(content)
+            result = read_strip_forces(content)
         elif self.extension == '.fe':
-            result = self._read_element_forces(content)
+            result = read_element_forces(content)
         elif self.extension == '.st':
-            result = self._read_stability_derivatives(content)
+            result = read_stability_derivatives(content)
         elif self.extension == '.sb':
-            result = self._read_body_derivatives(content)
+            result = read_body_derivatives(content)
         elif self.extension == '.hm':
-            result = self._read_hinge_moments(content)
+            result = read_hinge_moments(content)
         else:
             print("Unknown output file: {0}".format(self.path))
             result = []
         return result
 
-    def _read_totals(self, content):
-        return get_vars(content)
 
-    def _read_surface_forces(self, content):
+def read_totals(content):
+    return get_vars(content)
 
-        # Find start of table based on header
-        start_line, end_line = get_table_start_end(content,
-                                                   '(n\s+Area\s+CL)')
-        table_content = content[start_line:end_line]
 
-        surface_data = self._process_surface_table(table_content)
-        return surface_data
+def read_surface_forces(content):
 
-    def _process_surface_table(self, table_content):
-        header = extract_header(table_content)
-        surface_data = dict()
-        for line in table_content[1:]:
-            line_data = get_line_values(line)
+    # Find start of table based on header
+    start_line, end_line = get_table_start_end(content,
+                                               '(n\s+Area\s+CL)')
+    table_content = content[start_line:end_line]
 
+    surface_data = process_surface_table(table_content)
+    return surface_data
+
+
+def process_surface_table(table_content):
+    header = extract_header(table_content)
+    surface_data = dict()
+    for line in table_content[1:]:
+        line_data = get_line_values(line)
+
+        # ignore first column
+        line_data = line_data[1:]
+
+        name = re.findall('(\D+)(?=\n)', line)[0].strip()
+
+        if len(line_data) != len(header):
+            raise ParseError("Incorrect table format")
+
+        # Create results dictionary
+        # Combine surfaces labeled with (YDUP)
+        if '(YDUP)' in name:
+            base_name = remove_ydup(name)
+            base_data = surface_data[base_name]
+            all_data = zip(header, [base_data[key]
+                                    for key in header], line_data)
+            surface_data[base_name] = {key: base_value + value
+                                       for key, base_value, value
+                                       in all_data}
+        else:
+            surface_data[name] = {key: value
+                                  for key, value in zip(header,
+                                                        line_data)}
+    return surface_data
+
+
+def read_strip_forces(content):
+    table_content = _get_surface_tables(content)
+    strip_results = process_surface_tables(table_content)
+
+    return strip_results
+
+
+def process_surface_tables(table_content):
+    strip_results = dict()
+    # sort so (YDUP) surfaces are always behind the main surface
+    for name in sorted(table_content.keys()):
+        header = extract_header(table_content[name])
+
+        # check for YDUP
+        if '(YDUP)' in name:
+            result_name = remove_ydup(name)
+        else:
+            result_name = name
+            strip_results[result_name] = {key: [] for key in header}
+
+        for data_line in table_content[name][1:-1]:
+            # Convert to floats
+            values = get_line_values(data_line)
             # ignore first column
-            line_data = line_data[1:]
+            values = values[1:]
+            for key, value in zip(header, values):
+                strip_results[result_name][key].append(value)
+    return strip_results
 
-            name = re.findall('(\D+)(?=\n)', line)[0].strip()
 
-            if len(line_data) != len(header):
-                raise ParseError("Incorrect table format in file {0}"
-                                 .format(self.path))
+def _get_surface_tables(content):
+    table_lines = split_lines(content, SURFACE_RE)
+    table_dict = dict()
+    for name, lines in list(table_lines.items()):
+        re_str = '(j\s+Yle\s+Chord)'
+        start_line, end_line = get_table_start_end(lines, re_str)
+        if start_line is not None and end_line is not None:
+            table_dict[name] = lines[start_line:end_line]
+    return table_dict
 
-            # Create results dictionary
-            # Combine surfaces labeled with (YDUP)
-            if '(YDUP)' in name:
-                base_name = remove_ydup(name)
-                base_data = surface_data[base_name]
-                all_data = zip(header, [base_data[key]
-                                        for key in header], line_data)
-                surface_data[base_name] = {key: base_value + value
-                                           for key, base_value, value
-                                           in all_data}
-            else:
-                surface_data[name] = {key: value
-                                      for key, value in zip(header,
-                                                            line_data)}
-        return surface_data
 
-    def _read_strip_forces(self, content):
+def read_element_forces(content):
+    data_tables = get_element_tables(content)
+    element_results = process_element_tables(data_tables)
 
-        table_content = self._get_surface_tables(content)
-        strip_results = self.process_surface_tables(table_content)
+    return element_results
 
-        return strip_results
 
-    def process_surface_tables(self, table_content):
-        strip_results = dict()
-        # sort so (YDUP) surfaces are always behind the main surface
-        for name in sorted(table_content.keys()):
-            header = extract_header(table_content[name])
+def get_element_tables(content):
+    # tables split by surface
+    surface_tables = split_lines(content, SURFACE_RE)
+    data_tables = dict()
+    for surface_name, surface_lines in list(surface_tables.items()):
+        # tables split by strip
+        strip_tables = split_lines(surface_lines,
+                                   STRIP_RE)
+        data_tables[surface_name] = dict()
+        for strip_name, strip_lines in list(strip_tables.items()):
+            start_line, end_line = (get_table_start_end(
+                strip_lines, '(I\s+X\s+Y\s+Z)'))
+            data = strip_lines[start_line:end_line]
+            data_tables[surface_name][int(strip_name)] = data
+    return data_tables
 
-            # check for YDUP
-            if '(YDUP)' in name:
-                result_name = remove_ydup(name)
-            else:
-                result_name = name
-                strip_results[result_name] = {key: [] for key in header}
 
-            for data_line in table_content[name][1:-1]:
-                # Convert to floats
+def process_element_tables(data_tables):
+    element_results = dict()
+    # sort so (YDUP) surfaces are always behind the main surface
+    for name in sorted(data_tables.keys()):
+        # check for YDUP
+        if '(YDUP)' in name:
+            result_name = remove_ydup(name)
+        else:
+            result_name = name
+            element_results[result_name] = dict()
+
+        for strip in data_tables[name].keys():
+            header = extract_header(data_tables[name][strip])
+            # create empty lists
+            element_results[result_name][strip] = {key: []
+                                                   for key in header}
+            for data_line in data_tables[name][strip][1:]:
                 values = get_line_values(data_line)
                 # ignore first column
                 values = values[1:]
                 for key, value in zip(header, values):
-                    strip_results[result_name][key].append(value)
-        return strip_results
+                    element_results[result_name][strip][key].append(value)
+    return element_results
 
-    def _get_surface_tables(self, content):
-        table_lines = split_lines(content, self.SURFACE_RE)
-        table_dict = dict()
-        for name, lines in list(table_lines.items()):
-            re_str = '(j\s+Yle\s+Chord)'
-            start_line, end_line = get_table_start_end(lines, re_str)
-            if start_line is not None and end_line is not None:
-                table_dict[name] = lines[start_line:end_line]
-        return table_dict
 
-    def _read_element_forces(self, content):
-        data_tables = self._get_element_tables(content)
-        element_results = self._process_element_tables(data_tables)
+def read_stability_derivatives(content):
 
-        return element_results
+    idx = [i for (i, line) in enumerate(content)
+           if 'Stability-axis derivatives...' in line or
+           'Neutral point' in line]
 
-    def _get_element_tables(self, content):
-        # tables split by surface
-        surface_tables = split_lines(content, self.SURFACE_RE)
-        data_tables = dict()
-        for surface_name, surface_lines in list(surface_tables.items()):
-            # tables split by strip
-            strip_tables = split_lines(surface_lines,
-                                       self.STRIP_RE)
-            data_tables[surface_name] = dict()
-            for strip_name, strip_lines in list(strip_tables.items()):
-                start_line, end_line = (get_table_start_end(
-                    strip_lines, '(I\s+X\s+Y\s+Z)'))
-                data = strip_lines[start_line:end_line]
-                data_tables[surface_name][int(strip_name)] = data
-        return data_tables
+    all_vars = get_vars(content[idx[0]:idx[1]+1])
+    controls = get_controls(content)
+    all_vars = replace_controls(all_vars, controls)
 
-    def _process_element_tables(self, data_tables):
-        element_results = dict()
-        # sort so (YDUP) surfaces are always behind the main surface
-        for name in sorted(data_tables.keys()):
-            # check for YDUP
-            if '(YDUP)' in name:
-                result_name = remove_ydup(name)
-            else:
-                result_name = name
-                element_results[result_name] = dict()
+    return all_vars
 
-            for strip in data_tables[name].keys():
-                header = extract_header(data_tables[name][strip])
-                # create empty lists
-                element_results[result_name][strip] = {key: []
-                                                       for key in header}
-                for data_line in data_tables[name][strip][1:]:
-                    values = get_line_values(data_line)
-                    # ignore first column
-                    values = values[1:]
-                    for key, value in zip(header, values):
-                        element_results[result_name][strip][key].append(value)
-        return element_results
 
-    def _read_stability_derivatives(self, content):
+def read_body_derivatives(content):
+    idx = [i for (i, line) in enumerate(content)
+           if 'Geometry-axis derivatives...' in line]
 
-        idx = [i for (i, line) in enumerate(content)
-               if 'Stability-axis derivatives...' in line or
-               'Neutral point' in line]
+    all_vars = get_vars(content[idx[0]:])
+    controls = get_controls(content)
+    all_vars = replace_controls(all_vars, controls)
 
-        all_vars = get_vars(content[idx[0]:idx[1]+1])
-        controls = get_controls(content)
-        all_vars = replace_controls(all_vars, controls)
+    return all_vars
 
-        return all_vars
 
-    def _read_body_derivatives(self, content):
-        idx = [i for (i, line) in enumerate(content)
-               if 'Geometry-axis derivatives...' in line]
+def read_hinge_moments(content):
+    results = dict()
+    for line in content:
+        match = re.search('(\w+)\s+([-\dE.]+)', line)
+        if match is not None:
+            results[match.group(1)] = float(match.group(2))
+    return results
 
-        all_vars = get_vars(content[idx[0]:])
-        controls = get_controls(content)
-        all_vars = replace_controls(all_vars, controls)
-
-        return all_vars
-
-    @staticmethod
-    def _read_hinge_moments(content):
-        results = dict()
-        for line in content:
-            match = re.search('(\w+)\s+([-\dE.]+)', line)
-            if match is not None:
-                results[match.group(1)] = float(match.group(2))
-        return results
 
 def get_vars(content):
     # Search for "key = value" tuples and store in a dictionary
@@ -206,6 +218,7 @@ def get_vars(content):
         result[name] = float(value)
     return result
 
+
 def extract_header(table_content):
     # Get headers (might contain spaces, but no double spaces)
     header = re.split('\s{2,}', table_content[0])
@@ -214,6 +227,7 @@ def extract_header(table_content):
     # ignore first column
     header = header[1:]
     return header
+
 
 def split_lines(lines, re_str):
     splitted = dict()
@@ -228,12 +242,15 @@ def split_lines(lines, re_str):
 
     return splitted
 
+
 def get_line_values(data_line):
     values = [float(s) for s in re.findall('([-\dE.]+)', data_line)]
     return values
 
+
 def remove_ydup(name):
     return re.sub('\(YDUP\)', '', name).strip()
+
 
 def get_table_start_end(content, re_str):
     start_line, end_line = None, None
@@ -249,9 +266,11 @@ def get_table_start_end(content, re_str):
 
     return start_line, end_line
 
+
 def get_controls(content):
     controls = re.findall('(\S+)\s+(d\d+)', ''.join(content))
     return {number: name for (name, number) in controls}
+
 
 def replace_controls(var_dict, controls):
     # replace d# with control name
