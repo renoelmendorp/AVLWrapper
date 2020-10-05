b65647b05f8ef6702f8419e7b384ff419c21ff40
Reno Elmendorp <reno.elmendorp@icloud.com>
2020-10-05 15:28:21 +0200

    Updated __init__

--- avlwrapper/__init__.py
+++ avlwrapper/__init__.py
@@ -1,9 +1,11 @@
 """ AVLWrapper
 """
+VERSION = "0.3.0"
+
 from .config import default_config, Configuration
-from .model import (Aircraft, Control, DataAirfoil, DesignVar,
-                    FileAirfoil, NacaAirfoil, Point, ProfileDrag,
+from .model import (Aircraft, Case, Control, DataAirfoil, DesignVar,
+                    FileAirfoil, NacaAirfoil, Parameter, Point, ProfileDrag,
                     Section, Symmetry, Spacing, Surface, Vector)
 from .output import OutputReader
-from .session import Case, Parameter, Session
+from .session import Session
 from .tools import create_sweep_cases, partitioned_cases, show_image
