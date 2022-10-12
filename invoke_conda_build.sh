caca7b733207a46019ebf39e49a816fe36247df4
Phil Chiu <phil.chiu@archer.com>
2022-10-12 13:01:58 -0600

    Cleanup accidentally committed

--- invoke_conda_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION=`python setup.py --version` \
-	conda mambabuild \
-	# -c https://artifactory.int.flyarcher.com/artifactory/api/conda/conda \
-	-c conda-forge \
-	-c main \
-	conda.recipe
-
