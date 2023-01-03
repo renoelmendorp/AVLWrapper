a823359106af35d233624eccf80f5ccc0781c567
Reno Elmendorp <reno.elmendorp@icloud.com>
2023-01-03 22:37:09 +0100

    Added supra testcase

---
+++ tests/test_supra.py
@@ -0,0 +1,16 @@
+import os.path
+
+import pytest
+
+import avlwrapper as avl
+
+CDIR = os.path.dirname(os.path.realpath(__file__))
+RES_DIR = os.path.join(CDIR, "resources")
+
+
+def test_supra():
+    model = avl.Aircraft.from_file(os.path.join(RES_DIR, "supra.avl"))
+    cases = avl.Case.from_file(os.path.join(RES_DIR, "supra.run"))
+    session = avl.Session(geometry=model, cases=cases)
+    results = session.run_all_cases()
+    assert results
